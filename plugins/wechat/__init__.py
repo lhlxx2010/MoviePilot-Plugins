79db73ad4318eb96ed22831f6429eb2d51d5aae0
lhlxx2010 <kyriestars@gmail.com>
2024-02-26 01:13:03 +0800

    1.04

--- plugins/wechat/__init__.py
+++ plugins/wechat/__init__.py
@@ -15,7 +15,7 @@ class WeChat(_PluginBase):
     # 插件图标
     plugin_icon = "Wechat_A.png"
     # 插件版本
-    plugin_version = "1.03"
+    plugin_version = "1.04"
     # 插件作者
     plugin_author = "叉叉"
     # 作者主页
