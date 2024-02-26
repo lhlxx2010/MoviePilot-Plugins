335c332c3e5173fb552b9e96ad3a587ba12e3e23
lhlxx2010 <kyriestars@gmail.com>
2024-02-26 11:35:34 +0800

    1.07

--- plugins/wechat/__init__.py
+++ plugins/wechat/__init__.py
@@ -15,7 +15,7 @@ class WeChat(_PluginBase):
     # 插件图标
     plugin_icon = "Wechat_A.png"
     # 插件版本
-    plugin_version = "1.06"
+    plugin_version = "1.07"
     # 插件作者
     plugin_author = "叉叉"
     # 作者主页
@@ -63,7 +63,6 @@ class WeChat(_PluginBase):
                 "title": item.value,
                 "value": item.name
             })
-        # logger.info(f"config_url{self._wechat_url} 获取完成")
         return [
             {
                 'component': 'VForm',
@@ -167,7 +166,6 @@ class WeChat(_PluginBase):
         """
         消息发送事件
         """
-        # logger.info(f"启动send")
         try:
             if not self._wechat_url or not self._chatroomid:
                 logger.info(f"没有enable:{self._enabled}")
@@ -177,49 +175,37 @@ class WeChat(_PluginBase):
         except Exception as e:
             logger.error(f"发送事件中出现异常：{e}")
 
-        # logger.info(f"检查event")
         if not event.event_data:
             logger.info(f"没有event.event_data:{event.event_data}")
             return
 
 
         msg_body = event.event_data
-        # logger.info(f"msg_body:{msg_body}")
+        logger.info(f"msg_body:{msg_body}")
         # 渠道
         channel = msg_body.get("channel")
-        # logger.info(f"channel:{channel}")
         if channel:
             logger.info(f"channel空:{channel}")
             return
         # 类型
-        # logger.info(f"检查msg_type")
         msg_type: NotificationType = msg_body.get("type")
         logger.info(f"msg_type:{msg_type}")
         # 标题
-        # logger.info(f"检查title")
         title = msg_body.get("title")
-        # logger.info(f"title:{title}")
         # 文本
-        # logger.info(f"检查text")
         text = msg_body.get("text")
-        # logger.info(f"text:{text}")
         # 图像
         # image = msg_body.get("image")
 
         if not title and not text:
             logger.info("标题和内容不能同时为空")
             return
-        logger.info(f"text和title不为空通过")
-        logger.info(f'self._msgtypes = {self._msgtypes}')
-        logger.info(f'msg_type.name = {msg_type.name}')
         if (msg_type and self._msgtypes
                 and msg_type.name not in self._msgtypes):
             logger.info(f"消息类型 {msg_type.value} 未开启消息发送")
             return
         logger.info(f"消息类型检查通过")
-        # logger.info(f"开始try消息")
         try:
-            # logger.info(f"开始发消息")
             # if not image:
             payload = {
                 "para": {
@@ -261,6 +247,8 @@ class WeChat(_PluginBase):
             #     logger.warn(f"微信机器人消息发送失败，错误码：{res.status_code}，错误原因：{res.reason}")
             # else:
             #     logger.warn("微信机器人消息发送失败，未获取到返回信息")
+            else:
+                logger.warn(f"微信消息发送失败，错误：{res}")
         except Exception as msg_e:
             logger.error(f"微信机器人消息发送失败，{str(msg_e)}")
 
